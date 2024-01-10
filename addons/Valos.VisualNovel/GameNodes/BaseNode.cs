e9ae5b3aaa13f1e035177548adf5c9df0257a6a6
Sasa <sasa.seles@valosstudio.com>
2024-01-10 11:19:46 +0900

    Updates

--- addons/Valos.VisualNovel/GameNodes/BaseNode.cs
+++ addons/Valos.VisualNovel/GameNodes/BaseNode.cs
@@ -5,17 +5,17 @@ namespace Valos.VisualNovel.GameNodes;
 [Tool]
 public partial class BaseNode : GraphNode
 {
-    public override void _Ready()
-    {
-        GuiInput += OnGuiInput;
-    }
-
-    public void OnGuiInput(InputEvent @event)
-    {
-        if (Input.IsKeyPressed(Key.Delete) == true)
-        {
-            // EmitSignal(GraphElement.SignalName.DeleteRequest, this);
-            GD.PrintErr("Delete pressed");
-        }
-    }
+    // public override void _Ready()
+    // {
+    //     GuiInput += OnGuiInput;
+    // }
+    //
+    // public void OnGuiInput(InputEvent @event)
+    // {
+    //     if (Input.IsKeyPressed(Key.Delete) == true)
+    //     {
+    //         // EmitSignal(GraphElement.SignalName.DeleteRequest, this);
+    //         GD.PrintErr("Delete pressed");
+    //     }
+    // }
 }
