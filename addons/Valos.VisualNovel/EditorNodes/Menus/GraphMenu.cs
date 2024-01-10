30355ac899b7520674b33cbd275a4cbd08033253
Sasa <sasa.seles@valosstudio.com>
2024-01-10 10:26:29 +0900

    Crashes when creating a packed scene

--- addons/Valos.VisualNovel/EditorNodes/Menus/GraphMenu.cs
+++ addons/Valos.VisualNovel/EditorNodes/Menus/GraphMenu.cs
@@ -1,11 +1,52 @@
 using Godot;
+using Valos.VisualNovel.GameNodes;
 
 namespace Valos.VisualNovel.EditorNodes.Menus;
 
 [Tool]
 public partial class GraphMenu : PopupMenu
 {
+    [Signal]
+    public delegate void AddNodeEventHandler(BaseNode node, Vector2 gridPosition);
+
+    [Export()] public PackedScene DialogueNode { get; set; }
+    [Export()] public PackedScene ResponseNode { get; set; }
+    [Export()] public PackedScene LocationNode { get; set; }
+
+    private Vector2 gridPosition;
+
     public override void _Ready()
     {
+        IdPressed += OnIdPressed;
+    }
+
+    public void OnIdPressed(long id)
+    {
+        switch (id)
+        {
+            case 1:
+                SendNode(ResponseNode);
+                break;
+            case 2:
+                SendNode(LocationNode);
+                break;
+            default:
+                SendNode(DialogueNode);
+                break;
+        }
+    }
+
+    public void StartSelection(Vector2 gridPosition)
+    {
+        this.gridPosition = gridPosition;
+
+        this.Show();
+    }
+
+    private void SendNode(PackedScene packedScene)
+    {
+        BaseNode node = packedScene.Instantiate<BaseNode>();
+
+        EmitSignal(nameof(AddNode), node, gridPosition);
     }
 }
--- addons/Valos.VisualNovel/EditorNodes/TreeEditors/GraphEditor.cs
+++ addons/Valos.VisualNovel/EditorNodes/TreeEditors/GraphEditor.cs
@@ -1,11 +1,13 @@
 using Godot;
+using Valos.VisualNovel.EditorNodes.Menus;
+using Valos.VisualNovel.GameNodes;
 
 namespace Valos.VisualNovel.EditorNodes.TreeEditors;
 
 [Tool]
 public partial class GraphEditor : GraphEdit
 {
-    [Export()] public PopupMenu GraphMenu { get; set; }
+    [Export()] public GraphMenu GraphMenu { get; set; }
 
     public override void _Ready()
     {
@@ -16,18 +18,25 @@ public partial class GraphEditor : GraphEdit
 
     public void OnConnectionToEmpty(StringName fromNode, long fromPort, Vector2 releasePosition)
     {
-        ShowPopup();
+        ShowPopup(releasePosition);
     }
 
     public void OnPopupRequest(Vector2 position)
     {
-        ShowPopup();
+        ShowPopup(position);
     }
 
-    private void ShowPopup()
+    public void OnAddNode(BaseNode node, Vector2 gridPosition)
+    {
+        AddChild(node);
+
+        node.Position = gridPosition;
+    }
+
+    private void ShowPopup(Vector2 gridPosition)
     {
         GraphMenu.Position = (Vector2I)GetGlobalMousePosition() + GetWindow().Position;
 
-        GraphMenu.Show();
+        GraphMenu.StartSelection(gridPosition);
     }
 }
