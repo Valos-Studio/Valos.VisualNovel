b9e935617235d4cc79a84424fd778ce56a795afa
Sasa <sasa.seles@valosstudio.com>
2024-01-12 08:06:52 +0900

    NovelPanel update

--- addons/Valos.VisualNovel/GameNodes/NovelPanelCode.cs
+++ addons/Valos.VisualNovel/GameNodes/NovelPanelCode.cs
@@ -1,9 +1,41 @@
 using Godot;
+using Valos.VisualNovel.GameNodes.StartNodes;
 
 namespace Valos.VisualNovel.GameNodes;
 
 [Tool]
-[GlobalClass]
 public partial class NovelPanelCode : Node
 {
+    public StartNode StartNode { get; set; }
+    public Node DialogueNodes { get; set; }
+    public Node ResponseNodes { get; set; }
+    public Node LocationNodes { get; set; }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        DialogueNodes = InitNode(nameof(DialogueNodes));
+
+        ResponseNodes = InitNode(nameof(ResponseNodes));
+
+        LocationNodes = InitNode(nameof(LocationNodes));
+    }
+
+    // private StartNode InitStartNode()
+    // {
+    // }
+
+    private Node InitNode(string name)
+    {
+        Node property = new Node();
+
+        CallDeferred(Node.MethodName.AddChild, property);
+
+        property.SetDeferred(Node.PropertyName.Owner, this);
+
+        property.SetDeferred(Node.PropertyName.Name, name);
+
+        return property;
+    }
 }
