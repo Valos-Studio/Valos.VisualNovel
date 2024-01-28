a7d306ae52efac8b631e8013e99c94f5f6665b8c
Sasa <sasa.seles@valosstudio.com>
2024-01-28 10:53:11 +0900

    Cleaning up the graph editor

--- addons/Valos.VisualNovel/EditorNodes/TreeEditors/GraphEditor.cs
+++ addons/Valos.VisualNovel/EditorNodes/TreeEditors/GraphEditor.cs
@@ -14,40 +14,7 @@ public partial class GraphEditor : GraphEdit
 
     public override void _Ready()
     {
-        PopupRequest += OnPopupRequest;
-
-        ConnectionToEmpty += OnConnectionToEmpty;
-
-        DeleteNodesRequest += OnDeleteNodesRequest;
-
-        ConnectionRequest += OnConnectionRequest;
-
-        DisconnectionRequest += OnDisconnectionRequest;
-    }
-
-    public void OnDisconnectionRequest(StringName fromNode, long fromPort, StringName toNode, long toPort)
-    {
-        this.DisconnectNode(fromNode, (int)fromPort, toNode, (int)fromPort);
-    }
-
-    public void OnConnectionRequest(StringName fromNode, long fromPort, StringName toNode, long toPort)
-    {
-        this.ConnectNode(fromNode, (int)fromPort, toNode, (int)fromPort);
-    }
-
-    public async void OnConnectionToEmpty(StringName fromNode, long fromPort, Vector2 releasePosition)
-    {
-        GraphNode result = await ShowPopup(releasePosition);
-        
-        if (result != null)
-        {
-            OnConnectionRequest(fromNode, fromPort, result.Name, result.GetInputPortSlot(0));
-        }
-    }
-
-    public async void OnPopupRequest(Vector2 position)
-    {
-        await ShowPopup(position);
+        InitializeSignals();
     }
 
     public void OnDeleteNodesRequest(Array nodeNames)
---
+++ addons/Valos.VisualNovel/EditorNodes/TreeEditors/GraphEditorSignals.cs
@@ -0,0 +1,45 @@
+﻿using Godot;
+
+namespace Valos.VisualNovel.EditorNodes.TreeEditors;
+
+public partial class GraphEditor
+{
+
+    private void InitializeSignals()
+    {
+        PopupRequest += OnPopupRequest;
+        
+        ConnectionToEmpty += OnConnectionToEmpty;
+
+        DeleteNodesRequest += OnDeleteNodesRequest;
+
+        ConnectionRequest += OnConnectionRequest;
+
+        DisconnectionRequest += OnDisconnectionRequest;
+    }
+
+    public void OnDisconnectionRequest(StringName fromNode, long fromPort, StringName toNode, long toPort)
+    {
+        this.DisconnectNode(fromNode, (int)fromPort, toNode, (int)fromPort);
+    }
+
+    public void OnConnectionRequest(StringName fromNode, long fromPort, StringName toNode, long toPort)
+    {
+        this.ConnectNode(fromNode, (int)fromPort, toNode, (int)fromPort);
+    }
+
+    public async void OnConnectionToEmpty(StringName fromNode, long fromPort, Vector2 releasePosition)
+    {
+        GraphNode result = await ShowPopup(releasePosition);
+        
+        if (result != null)
+        {
+            OnConnectionRequest(fromNode, fromPort, result.Name, result.GetInputPortSlot(0));
+        }
+    }
+
+    public async void OnPopupRequest(Vector2 position)
+    {
+        await ShowPopup(position);
+    }
+}
